fdcc55a553766f4492632a48274055eebfdaf18d
trias-Astuti <64121412+trias-Astuti@users.noreply.github.com>
2020-04-22 13:25:59 +0700

    Menentukan bilangan ganjil genap

---
+++ BilanganGanjilGenap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	int a;
+	cout <<"masukkan nilai a";
+	cin >> a;
+	if (a % 2 == 1){
+		cout <<"Bilangan Ganjil ";
+	}else{
+		cout <<"Bilangan Genap ";
+	}
+}
